cb8013d00bbb199ee677ef0032f81415693c1079
Abidan Brito <43681148+abidanBrito@users.noreply.github.com>
2026-01-03 14:22:25 +0000

    feat: train XOR neural network

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include "neuron.hpp"
 #include "network.hpp"
 #include "activation.hpp"
+#include "criterion.hpp"
 
 #include <print>
 
@@ -8,17 +8,44 @@ using namespace ann;
 
 auto main() -> int
 {
-    Activation nonlinear_activation{.function = activation::tanh,
-                                    .derivative = activation::tanh_derivative};
-    Network net({2, 4, 1}, nonlinear_activation);
+    Activation activation{.function = activation::sigmoid,
+                          .derivative = activation::sigmoid_derivative};
+    Criterion criterion{.function = criterion::mse, .derivative = criterion::mse_derivative};
+    Network net({2, 4, 1}, activation, criterion);
 
-    constexpr std::array xor_inputs = {std::array{0.0, 0.0}, std::array{0.0, 1.0},
-                                       std::array{1.0, 0.0}, std::array{1.0, 1.0}};
+    // Training dataset (XOR truth table)
+    constexpr std::array training_data = {std::pair{std::array{0.0, 0.0}, std::array{0.0}},
+                                          std::pair{std::array{0.0, 1.0}, std::array{1.0}},
+                                          std::pair{std::array{1.0, 0.0}, std::array{1.0}},
+                                          std::pair{std::array{1.0, 1.0}, std::array{0.0}}};
 
-    for (const auto& input : xor_inputs)
+    // Training loop
+    std::println("--- Training XOR network ---\n");
+
+    constexpr int max_epochs = 750;
+    for (int epoch{1}; epoch < max_epochs; ++epoch)
     {
-        net.feed_forward({input[0], input[1]});
-        std::println("[{}, {}] -> {:.4f}", input[0], input[1], net.get_output()[0]);
+        double epoch_loss = 0.0;
+
+        for (const auto& [inputs, targets] : training_data)
+        {
+            net.feed_forward({inputs[0], inputs[1]});
+            epoch_loss += net.compute_loss({targets[0]});
+            net.back_propagate();
+            net.step(0.3, 0.75);
+        }
+
+        epoch_loss /= training_data.size();
+        std::println("Epoch {} | Loss: {:.4f}", epoch, epoch_loss);
+    }
+
+    // Test results
+    std::println("\n--- Testing trained network ---\n");
+    for (const auto& [inputs, targets] : training_data)
+    {
+        net.feed_forward({inputs[0], inputs[1]});
+        std::println("[{}, {}] -> {:.4f} (target: {:.1f})", inputs[0], inputs[1],
+                     net.get_output()[0], targets[0]);
     }
 
     return 0;
